7b93bbf85365a3a9c3a24f45f6873de5b992148d
Joshua Skelton <joshua.skelton@gmail.com>
2017-06-13 12:13:43 -0700

    Make font larger

--- game.py
+++ game.py
@@ -2,34 +2,21 @@ import tdl
 
 from player import Player
 
-tdl.set_font('terminal8x8_gs_ro.png')
+tdl.set_font('terminal32x32_gs_ro.png')
 console = tdl.init(40, 30, 'lunch break roguelike')
 
 p = Player()
 p.position = 10, 10
 
-map = [
-    ['#', '#', '#', '#'],
-    ['#', '', '', '#'],
-    ['#', '', '', '#'],
-    ['#', '', '', '#'],
-    ['#', '', '', '#'],
-    ['#', '', '', '#'],
-    ['#', '', '', '#'],
-    ['#', '#', '#', '#']
-]
-
+map = tdl.Console(40, 30)
+map.draw_char(0, 0, '#')
 
 running = True
 while running:
     console.clear()
 
 
-    for y in range(8):
-        for x in range(4):
-            if map[y][x]:
-                console.draw_char(x, y, map[y][x])
-
+    console.blit(map)
     p.draw(console)
 
     tdl.flush()
