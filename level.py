8bf28e17d712d383fcce627c82a868d3f47633b7
Joshua Skelton <joshua.skelton@gmail.com>
2017-06-14 12:55:10 -0700

    Basic Load/Save Functionality for Levels

--- level.py
+++ level.py
@@ -1,3 +1,6 @@
+import pickle
+import os
+
 import tdl
 
 class Level(object):
@@ -42,3 +45,33 @@ class Level(object):
 
             elif self.erasing:
                 self.draw_char(*pos, ' ')
+
+
+        if event.type == 'KEYDOWN':
+            if event.keychar.upper() == 'S':
+                self.save()
+
+            elif event.keychar.upper() == 'L':
+                self.load()
+
+    def save(self):
+        print('Saving Level Data')
+        data = [self.data.get_char(*i) for i in self.data]
+        binary_data = pickle.dumps(data)
+        with open('map.dat', 'wb') as file:
+            file.write(binary_data)
+
+    def load(self):
+        if not os.path.exists('map.dat'):
+            print('No Level Data Found to Load')
+            return
+
+        print('Loading Level Data')
+        with open('map.dat', 'rb') as file:
+            binary_data = file.read()
+
+        data = pickle.loads(binary_data)
+
+        for i, t in enumerate(self.data):
+            char, fg, bg = data[i]
+            self.data.draw_char(*t, chr(char), fg, bg)
