c16a652986c3fd540ab8bf6201212328857f2da4
Joshua Skelton <joshua.skelton@gmail.com>
2017-06-20 12:50:03 -0700

    Initial Implementation of ProgressBar

---
+++ ui/progressbar.py
@@ -0,0 +1,24 @@
+class ProgressBar(object):
+    def __init__(self, x, y, width, max, color):
+        self.max_value = max
+        self.current_value = max
+        self.width = width
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def update(self):
+        pass
+
+    def draw(self, console):
+        w = int(self.current_value / self.max_value * self.width)
+        v = '{}/{}'.format(self.current_value, self.max_value)
+        
+        for i in range(w):
+            c = ' '
+            if i < len(v):
+                c = v[i]
+            console.draw_char(self.x + i, self.y, c, bg=self.color)
+
+    def handle_events(self, event):
+        pass
--- ui/window.py
+++ ui/window.py
@@ -1,6 +1,8 @@
 import tdl
 
 import draw
+import palette
+from ui import progressbar
 
 class Window(object):
     def __init__(self, x, y, width, height, title=''):
@@ -10,6 +12,7 @@ class Window(object):
         self.height = height
         self.data = tdl.Console(width, height)
         self.title = title
+        self.children = []
 
     def update(self):
         draw.box(self.data, 0, 0, self.width, self.height)
@@ -20,9 +23,16 @@ class Window(object):
             x = max(center_of_box - center_of_title, 1)
             self.data.draw_str(x, 0, self.title[:self.width - 2])
 
+        for child in self.children:
+            child.update()
+
     def draw(self, console):
         self.update()
+        for child in self.children:
+            child.draw(self.data)
+
         console.blit(self.data, self.x, self.y)
 
     def handle_events(self, event):
-        pass
+        for child in self.children:
+            child.handle_events(event)
