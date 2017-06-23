286441c69d595fe12d029afbfac98f2da35a07d4
Joshua Skelton <joshua.skelton@gmail.com>
2017-06-23 12:04:00 -0700

    Fixing Twitch Chat Event Usage

--- twitchchatmanager.py
+++ twitchchatmanager.py
@@ -12,7 +12,7 @@ class TwitchChatManager(object):
         s = scene.Scene.current_scene
 
         if event.type == 'TWITCHCHATEVENT':
-            if event.command == 'PRIVMSG':
+            if event.message:
                 if event.message.upper() == '!JOIN':
                     p = player.Player(event.nickname[0], (1, 1), palette.colors[random.randint(1, len(palette.colors) - 1)])
                     p.nickname = event.nickname
